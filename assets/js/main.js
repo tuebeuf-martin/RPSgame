677ce071fa96bc4e1976db50a2b88ab60856ded0
tuebeuf-martin <martin.tuebeuf@gmail.com>
2020-11-23 15:39:43 +0100

    Design final final final, peut etre un peu de débugage necessaire mais la flemme

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,7 +1,7 @@
 $(document).ready(function(){
 //Ici je crée une fonction qui permet de deplacer mes elements//
     
-    $(".imgcard").draggable ({revert: "valid",revertDuration: 600,snap: true});
+    $(".imgcard").draggable({revert: "valid",revertDuration: 600});
     $(".dropzone").droppable ({snap: true,//Ici je peux les deposer//
         
       drop: function( event, ui ) {//Quand la fonction drop est appelé//
@@ -11,12 +11,16 @@ $(document).ready(function(){
     
 }); 
     var percentWins = 0;   
-    var draws = 0;
     var games = 0;
     var AIWin = 0;//Je defini mes variables (Je gagne ou l'ordinateur gagne)
     var PWin = 0;
     console.log(percentWins)
     
+    function resetCards() {//Ici je crée une fonction qui permet de remettre les carte sur la table a 0//
+      $("#AICard").attr("src", "card.png");
+      $("#PCard").attr("src", "assets/img/blank.png");
+    }
+
     function launch(choice) {
         games +=1;
         $("#games").html(games);
@@ -25,36 +29,39 @@ $(document).ready(function(){
     if (result == "win"){
         PWin +=1;
         $("#PCount").html(PWin);
+        $( ".AI" ).effect( "shake" );
+        $(".AI").attr("src", "assets/img/SadComp.png");
         setTimeout(
           function() 
           {
-            $("#AICard").attr("src", "card.png")
+            resetCards();
           }, 1500);
         
     }
     else if (result == "lose"){
         AIWin +=1;
         $("#IACount").html(AIWin);
+        $(".AI").attr("src", "assets/img/HappyComp.png");
         setTimeout(
           function() 
           {
-            $("#AICard").attr("src", "card.png")
+            resetCards();
           }, 1500);
         
     }
     else if (result == "Tie"){
         $('#IAChoice').html(" (Tie!)"); 
-        draws +=1;
         setTimeout(
           function() 
           {
-            $("#AICard").attr("src", "card.png")
+            resetCards();
           }, 1500);
         
     }
     else {
         console.log('Erreur : ' + result);
         alert('Something went wrong!');
+        resetCards();
     }
     
     percentWins = (PWin / games ) *100;
@@ -63,10 +70,10 @@ $(document).ready(function(){
   
   var compare = function(me, opponent) {//Conditions de victoire
     if(me === 'rock') {
-      
+      $("#PCard").attr("src", "Casino (4).jpg");//A chaque fois que la carte est posé, elle s'affiche sur la table//
         if(opponent === 'rock') {
-          $("#AICard").attr("src", "Casino (4).jpg");
-          $( "#AICard" ).effect( "shake" );
+          $("#AICard").attr("src", "Casino (4).jpg");//A chaque fois que la carte est posé, elle s'affiche sur la table//
+          $( "#AICard" ).effect( "shake" );//Ici la carte va se secouer quand il y a égalité//
             return 'Tie';
         } else if (opponent === 'paper') {
           $("#AICard").attr("src", "Casino (3).jpg");
@@ -77,6 +84,7 @@ $(document).ready(function(){
         }
       
     } else if (me === 'paper') {
+      $("#PCard").attr("src", "Casino (3).jpg");
       
         if(opponent === 'paper') {
           $("#AICard").attr("src", "Casino (3).jpg");
@@ -90,7 +98,7 @@ $(document).ready(function(){
           return 'lose';
       }
     } else if (me === 'scissors') {
-      
+      $("#PCard").attr("src", "Casino (2).jpg");
         if(opponent === 'scissors') {
           $("#AICard").attr("src", "Casino (2).jpg");
           $( "#AICard" ).effect( "shake" );
